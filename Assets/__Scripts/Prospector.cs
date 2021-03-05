31c0ed70a31b6c3a8c0cf868d1eabc30aed0cdd4
AngelM9840 <77748592+AngelM9840@users.noreply.github.com>
2021-03-04 23:06:59 -0500

    Prospector
    Prospector script utilizes Layout script which is attached to the Main Camera.

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -10,11 +10,13 @@ public class Prospector : MonoBehaviour {
 	static public Prospector 	S;
 
 	[Header("Set in Inspector")]
-	public TextAsset			deckXML;
+	public TextAsset deckXML;
+	public TextAsset layoutXML;
 
 
 	[Header("Set Dynamically")]
-	public Deck					deck;
+	public Deck	deck;
+	public Layout layout;
 
 	void Awake(){
 		S = this;
@@ -25,10 +27,14 @@ public class Prospector : MonoBehaviour {
 		deck.InitDeck (deckXML.text);
 		Deck.Shuffle(ref deck.cards);
 
-		Card c;  //调用Card脚本类
-        for(int cNum=0; cNum<deck.cards.Count; cNum++) {  //把洗牌后的卡，放到新的位置
-             c = deck.cards[cNum];
-             c.transform.localPosition = new Vector3((cNum%13)*3, cNum/13*4, 0);
-         }
-	}
+		//Card c;  
+        //for(int cNum=0; cNum<deck.cards.Count; cNum++) {  
+             //c = deck.cards[cNum];
+             //c.transform.localPosition = new Vector3((cNum%13)*3, cNum/13*4, 0);
+			 //}
+
+		layout = GetComponent<Layout>();
+        layout.ReadLayout(layoutXML.text);
+    }
 }
+
